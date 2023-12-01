84e2434c96fe14396e0df614bf5dc519a48770cc
Turmonkowl5 <88589289+Turmonkowl5@users.noreply.github.com>
2023-12-01 15:35:29 -0600

    Audio

--- scripts.js
+++ scripts.js
@@ -7,6 +7,9 @@ let ctx = canvas.getContext("2d");
 // Set the stage width and height to match that of the canvas
 stage.style.width = canvas.width + "px";
 stage.style.height = canvas.height + "px";
+// Button
+let startB = document.querySelector("#start");
+startB.addEventListener("click", clickHandler, false);
 //! Arrays
 let sprites = [];
 let assetsToLoad = [];
@@ -110,6 +113,30 @@ let background = null;
 let sleigh = null;
 // face
 let face = null;
+// Create logo
+let logo = Object.create(spriteObject);
+logo.sourceX = 5234;
+logo.sourceWidth = 192;
+logo.sourceHeight = 49;
+logo.width = 192;
+logo.height = 49;
+logo.x = 736;
+logo.y = 74;
+// Jump sound
+let jumpSound = document.querySelector("#jump");
+jumpSound.addEventListener("canplaythrough", loadHandler,false);
+jumpSound.load();
+assetsToLoad.push(jumpSound);
+// Score sound
+let scoreSound = document.querySelector("#score");
+scoreSound.addEventListener("canplaythrough", loadHandler, false);
+scoreSound.load();
+assetsToLoad.push(scoreSound);
+// music
+let music = document.querySelector("#music");
+music.addEventListener("canplaythrough", loadHandler, false);
+music.load();
+assetsToLoad.push(music);
 // facet update loop
 update();
 // ! Functions
@@ -124,13 +151,11 @@ function update(){
             render();
             break;
         case OVER:
+            sprites = [];
             endGame();
+            render();
             break;
         case BUILDING:
-            faces = [];
-            facesCollected = 0;
-            ices = [];
-            sprites = [];
             background = Object.create(spriteObject);
             background.sourceWidth = 1024;
             background.sourceHeight = 763;
@@ -139,14 +164,27 @@ function update(){
             sprites.push(background);
             buildMap(map1);
             buildMap(objects1);
+            break;
     }
 }
 // Called when things are loaded
 function loadHandler(){
     assetsLoaded ++;
     if(assetsLoaded === assetsToLoad.length){
+        image.removeEventListener("load", loadHandler, false);
+        jumpSound.removeEventListener("canplaythrough", loadHandler, false);
+        scoreSound.removeEventListener("canplaythrouhg", loadHandler, false);
+        music.removeEventListener("canplaythroughy", loadHandler, false);
+    }
+}
+// CLick handler for music
+function clickHandler(){
+    if(assetsLoaded === assetsToLoad.length){
+        music.volume = 1;
+        music.play();
         gameState = BUILDING;
     }
+    startB.remove();
 }
 // Called when game should be played
 function playGame(){
@@ -162,6 +200,9 @@ function playGame(){
     if(jump && sleigh.isOnGround){
         sleigh.vy += sleigh.jumpForce;
         sleigh.isOnGround = false;
+        jumpSound.currentTime = 0;
+        jumpSound.volume = 0.3;
+        jumpSound.play();
     }
     // Checks for no movement
     if(!moveLeft && !moveRight){
@@ -210,6 +251,9 @@ function playGame(){
     for(let i = 0; i < faces.length; i ++){
         let face = faces[i];
         if(hitTestRectangle(sleigh,face) && face.visible){
+            scoreSound.currentTime = 0;
+            scoreSound.volume = 0.8;
+            scoreSound.play();
             facesCollected ++;
             face.visible = false;
         }
@@ -221,10 +265,10 @@ function playGame(){
                 face.rF = true;
             }
             if(face.rF){
-                face.aRot += 0.7;
+                face.aRot += 0.5;
             }
             else{
-                face.aRot -= 0.7;
+                face.aRot -= 0.5;
             }
             face.rotation = face.aRot;
         }
@@ -281,11 +325,27 @@ function buildMap(array){
     gameState = PLAYING;
 }
 function endGame(){
-    render();
+    background.sourceX = 3716;
+    background.sourceWidth = 1518;
+    background.sourceHeight = 1139;
+    sprites.push(background);
+    sprites.push(logo);
+    if(logo.aRot >= 45){
+        logo.rF = false;
+    }
+    else if(logo.aRot<= -45){
+        logo.rF = true;
+    }
+    if(logo.rF){
+        logo.aRot += 0.7;
+    }
+    else{
+        logo.aRot -= 0.7;
+    }
+    logo.rotation = logo.aRot;
 }
 function render(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    if(gameState != OVER){
         for(let i = 0; i < sprites.length; i ++){
             let sprite = sprites[i];
             ctx.save();
@@ -321,13 +381,5 @@ function render(){
                 }
                 ctx.restore();
             }
-        }
-    }
-    else{
-        ctx.drawImage(
-            image,
-            Math.floor(background.sourceX), Math.floor(background.sourceY), Math.floor(background.sourceWidth), Math.floor(background.sourceHeight),
-            Math.floor(background.x), Math.floor(background.y), Math.floor(background.width), Math.floor(background.height)
-        );
-    }
+        }    
 }
